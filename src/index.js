2736eb020b213828115bf1dd8046b80ef8783f3f
Erik Muir <erikdmuir@gmail.com>
2020-08-17 21:14:04 -0500

    add index.js and dependencies

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { LogEnv, LambdaLogger, LambdaResponse } = require('@erikmuir/lambda-utils');
+
+exports.handler = async function (event, context) {
+  LogEnv.lambdaEvent = event;
+  LogEnv.lambdaContext = context;
+  const logger = new LambdaLogger('index');
+  const response = new LambdaResponse();
+
+  try {
+    const body = {};
+    logger.info(body);
+    response.statusCode = 200;
+    response.body = body;
+  } catch (e) {
+    logger.error(e);
+    response.statusCode = 401;
+    response.body = { error: e.message };
+  }
+
+  return response.build();
+};
