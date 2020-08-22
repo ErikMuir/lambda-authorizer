7b83de131f5635de2360ef1dcd1ca5ca94e515a5
Erik Muir <erikdmuir@gmail.com>
2020-08-21 22:25:58 -0500

    add RequestValidator

---
+++ src/Services/RequestValidator.js
@@ -0,0 +1,59 @@
+import { LambdaLogger } from '@erikmuir/lambda-utils';
+import ApiGatewayArn from '../Models/ApiGatewayArn';
+import ApiGatewayArnException from '../Exceptions/ApiGatewayArnException';
+import RequestValidationException from '../Exceptions/RequestValidationException';
+
+export default class RequestValidator {
+  constructor(request) {
+    this._logger = new LambdaLogger('RequestValidationService');
+    this._request = request;
+    this._errors = [];
+    this._apiGatewayArn = null;
+    this._authTokenPattern = RegExp("^Bearer .*$");
+  }
+
+  get apiGatewayArn() { return this._apiGatewayArn; }
+
+  validate() {
+    this._logger.trace('Validating request');
+
+    if (!this._request) {
+      this._errors.push('Request is required.');
+    } else {
+      const { type, authorizationToken, methodArn } = this._request;
+
+      if (!type) {
+        this._errors.push('Expected "type" to have been provided.');
+      } else if (type.toUpperCase() !== 'TOKEN') {
+        this._errors.push('Expected "type" to have value "TOKEN".');
+      }
+
+      if (!authorizationToken) {
+        this._errors.push('Expected "authorizationToken" to have been provided.');
+      } else if (!this._authTokenPattern.test(authorizationToken)) {
+        this._errors.push('Expected "authorizationToken" to match "^Bearer .*$".');
+      }
+
+      if (!methodArn) {
+        this._errors.push('Expected "methodArn" to have been provided.');
+      } else {
+        try {
+          this._apiGatewayArn = new ApiGatewayArn(methodArn);
+        } catch (e) {
+          if (e instanceof ApiGatewayArnException) {
+            this._errors.push(`Could not parse "methodArn": "${methodArn}".`);
+          } else {
+            throw e;
+          }
+        }
+      }
+    }
+
+    if (this._errors.length > 0) {
+      this._logger.error('Request validation failed', { requestValidationErrors: this._errors });
+      throw new RequestValidationException();
+    }
+
+    this._logger.trace('Successfully validated request');
+  }
+}
---
+++ tests/Services/RequestValidator.spec.js
@@ -0,0 +1,69 @@
+import RequestValidator from '../../src/Services/RequestValidator';
+import RequestValidationException from '../../src/Exceptions/RequestValidationException';
+import ApiGatewayArn from '../../src/Models/ApiGatewayArn';
+
+jest.mock('@erikmuir/lambda-utils/dist/utilities/LambdaLogger');
+
+describe('RequestValidator', () => {
+  const type = 'TOKEN';
+  const authorizationToken = 'Bearer xyz';
+  const methodArn = 'arn:partition:service:region:aws-account-id:rest-api-id/stage/verb/path/to/resource';
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe('validate', () => {
+    describe('does not throw', () => {
+      test('when type, authorizationToken, and methodArn are valid', () => {
+        const request = { type, authorizationToken, methodArn };
+        const validator = new RequestValidator(request);
+
+        try {
+          validator.validate();
+
+          expect(true).toBe(true);
+        } catch (e) {
+          expect(true).toBe(false); // should never happen
+        }
+      });
+    });
+    
+    describe('throws RequestValidationException', () => {
+      [
+        { request: undefined, desc: 'when request is undefined' },
+        { request: null, desc: 'when request is null' },
+        { request: { authorizationToken, methodArn }, desc: 'when type not provided' },
+        { request: { type: 'bad-type', authorizationToken, methodArn }, desc: 'when type is not "TOKEN"' },
+        { request: { type, methodArn }, desc: 'when authorizationToken not provided' },
+        { request: { type, authorizationToken: 'bad-token', methodArn }, desc: 'when authorizationToken does not match "^Bearer .*$"' },
+        { request: { type, authorizationToken }, desc: 'when methodArn not provided' },
+        { request: { type, authorizationToken, methodArn: 'bad-arn' }, desc: 'when methodArn cannot be parsed' },
+      ].forEach(({ request, desc }) => {
+        test(desc, () => {
+          const validator = new RequestValidator(request);
+
+          try {
+            validator.validate();
+
+            expect(true).toBe(false); // should never happen
+          } catch (e) {
+            expect(e instanceof RequestValidationException).toBe(true);
+          }
+        });
+      });
+    });
+  });
+
+  test('apiGatewayArn getter', () => {
+    const request = { type, authorizationToken, methodArn };
+    const validator = new RequestValidator(request);
+    validator.validate();
+
+    const apiGatewayArn = validator.apiGatewayArn;
+
+    expect(apiGatewayArn).toBeDefined();
+    expect(apiGatewayArn instanceof ApiGatewayArn).toBe(true);
+    expect(apiGatewayArn.toString()).toBe(methodArn);
+  });
+});
